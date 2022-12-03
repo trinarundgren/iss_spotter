544fae4359dba0f57d9f90a767fac633d4c1ce55
Trina <trina.rundgren@telus.com>
2022-12-02 19:54:34 -0800

    final version

---
+++ index2.js
@@ -0,0 +1,16 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+// Call 
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
